37856a71831164d9b4a10646ccd98365e43e68a9
whiterose666 <danuandreanid@gmail.com>
2020-07-01 19:13:47 +0700

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/danu/catkin_custom/catkin_gazebo_simple/src
+# Build directory: /home/danu/catkin_custom/catkin_gazebo_simple/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(my_simulations)
---
+++ build/my_simulations/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/danu/catkin_custom/catkin_gazebo_simple/src/my_simulations
+# Build directory: /home/danu/catkin_custom/catkin_gazebo_simple/build/my_simulations
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
